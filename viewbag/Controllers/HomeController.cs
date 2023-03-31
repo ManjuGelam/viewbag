36c7ed44cb9514f8218f2863626f66d41197e545
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-31 15:34:47 +0530

    Add project files.

---
+++ viewbag/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace viewbag.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult index1(int? id, string name, double? price)
+        {
+            ViewBag.id = id;
+            ViewBag.name = name;
+            ViewBag.price = price;
+            return View();
+        }
+        public ViewResult display2()
+        {
+            List<string> food = new List<string>() { " biryani", "lemon rice", "gulabjaamon", "panipuri", "noodles" };
+                ViewBag.food = food;
+            return View();
+        }
+    }
+}
